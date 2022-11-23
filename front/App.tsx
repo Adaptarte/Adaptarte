ef218fafb711192e05e5b834d1c58070d4f68eaf
estidlore <estidlore@outlook.com>
2022-11-23 18:01:54 -0500

    react-native ts setup

---
+++ front/App.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const App: FC = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>{"Adaptarte"}</Text>
+    </SafeAreaView>
+  );
+};
+
+export { App };
