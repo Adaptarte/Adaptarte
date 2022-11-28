0997c332abab1e78f16f4df5e7bdda58dd62fdc3
JuanCarri007 <71296301+JuanCarri007@users.noreply.github.com>
2022-11-28 16:40:49 -0500

    feat(front): #11 landing images
    - Images module declaration
    - Added landing screen images

---
+++ front/src/assets/imgs/index.ts
@@ -0,0 +1,19 @@
+import calm from "./Calm.png";
+import dataDose from "./DataDose.png";
+import diet from "./Diet.png";
+import diseaseRegister from "./DiseaseRegister.png";
+import drinkingWater from "./DrinkingWater.png";
+import exercise from "./Exercise.png";
+import pills from "./Pills.png";
+import profile from "./Profile.png";
+
+export { 
+  calm, 
+  dataDose, 
+  diet, 
+  diseaseRegister, 
+  drinkingWater, 
+  exercise, 
+  pills,
+  profile 
+};
