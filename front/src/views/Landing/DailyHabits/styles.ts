f756310a237cec34c0ab75b6611f2bcc55a78fa7
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2022-11-30 16:02:29 -0500

    fix(front): #22 daily habits (#23)
    - Use wrappers for columns
    - Use spacing between columns only

--- front/src/views/Landing/DailyHabits/styles.ts
+++ front/src/views/Landing/DailyHabits/styles.ts
@@ -3,14 +3,16 @@ import { StyleSheet } from "react-native";
 import { colors } from "styles";
 
 const styles = StyleSheet.create({
+  column: {
+    padding: 8,
+    width: "50%",
+  },
   container: {
     backgroundColor: colors.WHITE,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "center",
-    paddingTop: 15,
-    width: "100%",
+    padding: -8,
   },
 });
 
