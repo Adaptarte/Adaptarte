070750762d983a7b2b2518d5576bfd0292dbbd93
estidlore <estidlore@outlook.com>
2022-12-03 13:53:02 -0500

    feat(front): #4 column and row components
    - Add grid system with column and row
    - Only columns amount in row needed
    - Custom space between columns

---
+++ front/src/components/Grid/Column/types.ts
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+interface IColumnProps {
+  children?: ReactNode;
+}
+
+export type { IColumnProps };
---
+++ front/src/components/Grid/Row/index.tsx
@@ -0,0 +1,27 @@
+import type { FC } from "react";
+import { createContext } from "react";
+import React from "react";
+import { View } from "react-native";
+
+import { styles } from "./styles";
+import type { IRowProps } from "./types";
+
+const RowContext = createContext({ columns: 1, spacing: 8 });
+
+const Row: FC<IRowProps> = ({
+  children,
+  columns = 1,
+  spacing = 8,
+}: IRowProps): JSX.Element => {
+  const margin = spacing / -2;
+
+  return (
+    <RowContext.Provider value={{ columns, spacing }}>
+      <View style={[styles.container, { margin }]}>
+        {children}
+      </View>
+    </RowContext.Provider>
+  );
+};
+
+export { Row, RowContext };
