a65ff49c2bd1d76d39be71d424d53b53b065392f
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2022-12-04 15:31:40 -0500

    feat(front): #4 column and row components (#32)
    - Add grid system with column and row
    - Only columns amount in row needed
    - Custom space between columns

---
+++ front/src/components/Grid/Column/index.tsx
@@ -0,0 +1,21 @@
+import type { FC } from "react";
+import { useContext } from "react";
+import React from "react";
+import { View } from "react-native";
+
+import { RowContext } from "../Row";
+import type  { IColumnProps } from "./types";
+
+const Column: FC<IColumnProps> = ({
+  children,
+}: IColumnProps): JSX.Element => {
+  const { columns, spacing } = useContext(RowContext);
+  const padding = spacing / 2;
+  const width = `${100 / columns}%`;
+
+  return (
+    <View style={{ padding, width }}>{children}</View>
+  );
+};
+
+export { Column };
---
+++ front/src/components/Grid/Row/types.ts
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+interface IRowProps {
+  children?: ReactNode;
+  columns?: number;
+  spacing?: number;
+}
+
+export type { IRowProps };
