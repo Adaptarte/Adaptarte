121ec1cd1aca0973f1f0d67892975bd4ddb27a88
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2022-12-05 21:27:16 -0500

    feat(front): #41 feeding texts (#43)
    - Add Feeding translations
    - Add Feeding texts
    - Add Feeding text styles

---
+++ front/src/views/Feeding/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import { colors, font } from "styles";
+
+const styles = StyleSheet.create({
+  description: {
+    color: colors.BLACK,
+    fontSize: font.sizes[2],
+    marginBottom: 16,
+  },
+  title: {
+    color: colors.GLAUCOUS,
+    fontSize: font.sizes[3],
+    fontWeight: "bold",
+    marginBottom: 8,
+    marginTop: 32,
+  },
+});
+
+export { styles };
