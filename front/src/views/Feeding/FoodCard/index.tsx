38c1a437777d19b79e690b4ea68288db0c267cbe
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2022-12-05 21:34:51 -0500

    feat(front): #41 feeding food cards (#44)
    - Add FoodCard for Feeding
    - Add Feeding liquids example cards

---
+++ front/src/views/Feeding/FoodCard/index.tsx
@@ -0,0 +1,24 @@
+import type { FC } from "react";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+import { Column } from "components/Grid";
+
+import { styles } from "./styles";
+import type { IFoodCardProps } from "./types";
+
+const FoodCard: FC<IFoodCardProps> = ({
+  image,
+  name,
+}: IFoodCardProps): JSX.Element => {
+  return (
+    <Column>
+      <View style={styles.container}>
+        <Image source={image} style={styles.image} />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+    </Column>
+  );
+};
+
+export { FoodCard };
---
+++ front/src/views/Feeding/FoodCard/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+import { colors, font } from "styles";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE,
+    borderColor: colors.LIGHT,
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 8,
+  },
+  image: {
+    aspectRatio: 1.5,
+    height: undefined,
+    resizeMode: "contain",
+    width: "100%",
+  },
+  name: {
+    color: colors.BLACK,
+    fontSize: font.sizes[2],
+    fontWeight: "bold",
+    marginTop: 8,
+    textAlign: "center",
+  },
+});
+
+export { styles };
