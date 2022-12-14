97518bd45b998174a1658d65b4322f4c715dbe57
estidlore <estidlore@outlook.com>
2022-12-14 02:47:28 -0500

    feat(front): #41 consumption view
    - Add Consumption view
    - Add Consumption to app navigation

---
+++ front/src/views/Consumption/styles.ts
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+import { colors, font } from "styles";
+
+const styles = StyleSheet.create({
+  cancelButton: {
+    marginTop: 16,
+  },
+  foodAddButton: {
+    alignSelf: "center",
+    backgroundColor: colors.GLAUCOUS,
+    borderRadius: 8,
+    padding: 8,
+  },
+  foodAddImage: {
+    height: 16,
+    tintColor: colors.WHITE,
+    width: 16,
+  },
+  foodImage: {
+    aspectRatio: 1.5,
+    height: undefined,
+    marginBottom: 4,
+    resizeMode: "contain",
+    width: "100%",
+  },
+  foodName: {
+    color: colors.BLACK,
+    fontSize: font.sizes[2],
+    marginBottom: 8,
+    textAlign: "center",
+  },
+  title: {
+    color: colors.GLAUCOUS,
+    fontSize: font.sizes[3],
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+});
+
+export { styles };
