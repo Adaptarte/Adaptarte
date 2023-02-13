ddf14cba724c53a4df385133a1231424adaef972
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-02-13 14:11:00 -0500

    refac(front): #58 checkbox comply eslint (#78)
    - Add type to create variant styles easily
    - Remove redundant checkbox props
    - Clean CheckBox code for variants

---
+++ front/src/styles/types.ts
@@ -0,0 +1,7 @@
+import type { ImageStyle, StyleSheet, TextStyle, ViewStyle  } from "react-native";
+
+type VarStyle<T extends string> = StyleSheet.NamedStyles<
+  Record<T, ImageStyle | TextStyle | ViewStyle>
+>;
+
+export type { VarStyle };
