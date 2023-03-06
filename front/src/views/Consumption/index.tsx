1d06fbc401be5325b562bcecc2319ddf106d6e33
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-03-06 16:52:48 -0500

    fix(front): #70 show only asked consumption type
    - Show options only for the selected food type in Consumption

--- front/src/views/Consumption/index.tsx
+++ front/src/views/Consumption/index.tsx
@@ -8,19 +8,22 @@ import { Column, Row } from "components/Grid";
 import { Screen } from "components/Screen";
 import type { TAppViewProps } from "navigation/App/types";
 import { colors } from "styles";
-import { food } from "utils/data";
+import { getFoodByType } from "utils/data";
 
 import { styles } from "./styles";
 import { t } from "./translations";
 
 const Consumption: FC<TAppViewProps<"Consumption">> = ({
-  navigation: { canGoBack, goBack }
+  navigation: { canGoBack, goBack },
+  route: { params }
 }: TAppViewProps<"Consumption">): JSX.Element => {
+  const { type = "liquids" } = params;
+
   return (
     <Screen style={{ backgroundColor: colors.WHITE }}>
       <Text style={styles.title}>{t().title}</Text>
       <Row columns={3}>
-        {food.map(
+        {getFoodByType(type).map(
           (el): JSX.Element => (
             <Column key={el.name}>
               <Image source={el.image} style={styles.foodImage} />
