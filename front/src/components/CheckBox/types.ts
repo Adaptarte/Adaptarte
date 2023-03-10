755ecce57737f5fa444985aeee3af83bcc710f1b
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-03-10 09:47:39 -0500

    feat(front): #100 allow to disable checkbox (#102)
    - Add disabled prop to Checkbox for the Touchable view

--- front/src/components/CheckBox/types.ts
+++ front/src/components/CheckBox/types.ts
@@ -1,6 +1,7 @@
 type CheckBoxVariant = "circle" | "rounded";
 
 interface CheckBoxProps {
+  disabled?: boolean;
   isChecked?: boolean;
   onChange?: (val: boolean) => void;
   variant?: CheckBoxVariant;
