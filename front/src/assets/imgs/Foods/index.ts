1789a4ab5047ffb594bc16d994f8b450fb2ae49d
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-03-13 11:56:40 -0500

    feat(front): #11 add images remaining (#109)
    - Add imgs remaining for each type
    - Create new folder for each imgs type inside Food folder

--- front/src/assets/imgs/Foods/index.ts
+++ front/src/assets/imgs/Foods/index.ts
@@ -1,23 +1,51 @@
-import bread from "./Bread.png";
-import chocolate from "./Chocolate.png";
-import coffee from "./Coffee.png";
-import juice from "./Juice.png";
-import milk from "./Milk.png";
-import pineapple from "./Pineapple.png";
-import potato from "./Potato.png";
-import rice from "./Rice.png";
-import tea from "./Tea.png";
-import water from "./Water.png";
-import watermelon from "./Watermelon.png";
-import wine from "./Wine.png";
-import yogurt from "./Yogurt.png";
+import apple from "./FruitsAndVegetables/Apple.png";
+import arepa from "./Carbs/Arepa.png";
+import banana from "./FruitsAndVegetables/Banana.png";
+import bread from "./Carbs/Bread.png";
+import cassava from "./Carbs/Cassava.png";
+import cherry from "./FruitsAndVegetables/Cherry.png";
+import chocolate from "./Dairy/Chocolate.png";
+import coffee from "./Dairy/Coffee.png";
+import cookie from "./Carbs/Cookie.png";
+import corn from "./Carbs/Corn.png";
+import empanada from "./Carbs/Empanada.png";
+import grape from "./FruitsAndVegetables/Grape.png";
+import juice from "./Liquids/Juice.png";
+import kiwi from "./FruitsAndVegetables/Kiwi.png";
+import lemonade from "./Liquids/Lemonade.png";
+import milk from "./Dairy/Milk.png";
+import orange from "./FruitsAndVegetables/Orange.png";
+import papaya from "./FruitsAndVegetables/Papaya.png";
+import pasta from "./Carbs/Pasta.png";
+import pineapple from "./FruitsAndVegetables/Pineapple.png";
+import potato from "./Carbs/Potato.png";
+import rice from "./Carbs/Rice.png";
+import tea from "./Liquids/Tea.png";
+import water from "./Liquids/Water.png";
+import watermelon from "./FruitsAndVegetables/Watermelon.png";
+import wine from "./Liquids/Wine.png";
+import yogurt from "./Dairy/Yogurt.png";
 
 const foodimgs = {
+  apple,
+  arepa,
+  banana,
   bread,
+  cassava,
+  cherry,
   chocolate,
   coffee,
+  cookie,
+  corn,
+  empanada,
+  grape,
   juice,
+  kiwi,
+  lemonade,
   milk,
+  orange,
+  papaya,
+  pasta,
   pineapple,
   potato,
   rice,
