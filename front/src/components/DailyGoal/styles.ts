d716243afe8f3378ef066cc61b4e41f69071feac
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-03-24 18:25:38 -0500

    refac(front): #123 clean dailygoal code
    - Remove containers and styles that do nothing
    - Make some styles responsive

--- front/src/components/DailyGoal/styles.ts
+++ front/src/components/DailyGoal/styles.ts
@@ -4,59 +4,28 @@ import type { TextVariants } from "components/Text/types";
 import { colors } from "styles";
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     alignItems: "center",
     backgroundColor: colors.YELLOW,
-    borderColor: colors.TRANSPARENT,
-    borderRadius: 20,
+    borderRadius: 16,
     borderWidth: 1,
     display: "flex",
     elevation: 4,
     flexDirection: "row",
-    height: 70,
-    justifyContent: "center",
     marginBottom: 20,
-    marginLeft: "auto",
-    marginRight: "auto",
-    shadowColor: "#000",
-    shadowOffset: {
-      height: 5,
-      width: 0
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-    width: "100%"
-  },
-  checkBox: {
-    marginLeft: "6%",
-    marginRight: "auto",
-    position: "relative",
-    width: "15%",
-    zIndex: -1
-  },
-  container: {
-    width: "25%"
+    paddingHorizontal: 12,
+    paddingVertical: 8
   },
   content: {
-    width: "55%"
-  },
-  contentContainer: {
-    width: "100%"
-  },
-  elipse: {
-    backgroundColor: colors.PURPLE_TRANSLUCID,
-    borderRadius: 200,
-    height: 50,
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: 50
+    flexBasis: 1,
+    flexGrow: 1,
+    marginHorizontal: 16
   },
   img: {
-    height: 54,
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: 6,
-    width: 38
+    borderRadius: 25,
+    height: 50,
+    resizeMode: "contain",
+    width: 50
   }
 });
 
