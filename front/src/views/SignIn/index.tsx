6cac74f71e3962c6b6395d1d95cf8f9e6e6f8106
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-11 16:43:07 -0500

    feat(front): #170 add signin screen
    - Add basic SignIn screen
    - Handle Google sign-in

---
+++ front/src/views/SignIn/index.tsx
@@ -0,0 +1,49 @@
+import React, { useCallback } from "react";
+import { Image, View } from "react-native";
+
+import { imgs } from "assets/imgs";
+import { Button } from "components/Button";
+import { Input } from "components/Input";
+import { Screen } from "components/Screen";
+import { Text } from "components/Text";
+import { signInGoogle } from "utils/auth";
+
+import { btnVars, styles, textVars } from "./styles";
+
+const SignIn = (): JSX.Element => {
+  const handleSignInGoogle = useCallback(() => {
+    signInGoogle()
+      .then(() => {
+        console.log("Signed in with Google!");
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <Screen>
+      <Text variant={textVars.appName}>{"Adaptarte"}</Text>
+      <View style={styles.header}>
+        <Image source={imgs.helloCuidador} style={styles.img} />
+      </View>
+      <Text variant={textVars.signIn}>{"Inicia sesión"}</Text>
+      <Input label={"Correo electrónico"} />
+      <Input label={"Contraseña"} />
+      <Button style={styles.btn} variant={btnVars.signIn}>
+        {"Iniciar sesión"}
+      </Button>
+      <View style={styles.hr} />
+      <Button
+        onPress={handleSignInGoogle}
+        style={styles.btn}
+        variant={btnVars.thirdParty}
+      >
+        {"Ingresar con Google"}
+      </Button>
+      <Button style={styles.btn} variant={btnVars.thirdParty}>
+        {"Ingresar con Apple"}
+      </Button>
+    </Screen>
+  );
+};
+
+export { SignIn };
