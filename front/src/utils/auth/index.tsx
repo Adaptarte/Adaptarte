63791650b22ceda22033f4eb859f8ca3549636cf
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-04-11 14:23:39 -0500

    feat(front): #132 add auth utils (#168)
    - Setup GoogleSignIn
    - Add useAuth to listen firebase user
    - Add UserContext with provider and hook for signed user

---
+++ front/src/utils/auth/index.tsx
@@ -0,0 +1,39 @@
+import type { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import auth from "@react-native-firebase/auth";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { createContext, useContext, useEffect, useState } from "react";
+
+GoogleSignin.configure({
+  webClientId:
+    "288895389085-2uqd4dqq95l9r46jqm2p533jkidn3lph.apps.googleusercontent.com"
+});
+
+const signInGoogle = async (): Promise<FirebaseAuthTypes.UserCredential> => {
+  await GoogleSignin.hasPlayServices();
+  const token = await GoogleSignin.signIn();
+  const credential = auth.GoogleAuthProvider.credential(token.idToken);
+  return auth().signInWithCredential(credential);
+};
+
+const useAuth = (): FirebaseAuthTypes.User | null | undefined => {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+  useEffect(() => {
+    return auth().onAuthStateChanged(setUser);
+  }, []);
+  return user;
+};
+
+const UserContext = createContext<FirebaseAuthTypes.User | null>(null);
+
+const UserProvider =
+  UserContext.Provider as React.Provider<FirebaseAuthTypes.User>;
+
+const useUser = (): FirebaseAuthTypes.User => {
+  const ctx = useContext(UserContext);
+  if (ctx === null) {
+    throw Error("User error. UserProvider not found.");
+  }
+  return ctx;
+};
+
+export { signInGoogle, useAuth, UserProvider, useUser };
