11c75bfdd778cfe06cafc37919ff98104a419807
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-04-11 14:27:49 -0500

    feat(front): #165 add splash screen (#169)
    - Move Screen's prop style from the SafeArea to the Scroll
    - Add Splash screen

---
+++ front/src/views/Splash/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+import type { TextVariants } from "components/Text/types";
+import { colors } from "styles";
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    display: "flex",
+    flex: 1,
+    justifyContent: "center"
+  },
+  img: {
+    backgroundColor: colors.BLUE,
+    borderRadius: 96,
+    height: 128,
+    margin: 32,
+    position: "absolute",
+    resizeMode: "cover",
+    width: 128
+  }
+});
+
+const textVars: TextVariants<"header"> = {
+  header: {
+    color: "BLUE",
+    size: 5,
+    weight: "bold"
+  }
+};
+
+export { styles, textVars };
