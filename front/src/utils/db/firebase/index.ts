f4997b7fcab3dd0940043764556ff66ab04f5d34
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-13 18:44:26 -0500

    feat(front): #163 add firebase utils
    - Add types Query and QueryFilter
    - Add refUserCol to reference an user collection
    - Add delUserData to delete a doc from an user collection

--- front/src/utils/db/firebase/index.ts
+++ front/src/utils/db/firebase/index.ts
@@ -1,11 +1,7 @@
 import firestore from "@react-native-firebase/firestore";
 
 import type { DBUser, DBUserCollections } from "../types";
-import type { DocRef } from "./types";
-
-const refUser = (id: string): DocRef<DBUser> => {
-  return firestore().collection<DBUser>("Users").doc(id);
-};
+import type { ColRef, DocRef } from "./types";
 
 const addUser = async (id: string): Promise<void> => {
   const snapshot = await refUser(id).get();
@@ -21,8 +17,26 @@ const addUserData = async <T extends keyof DBUserCollections>(
   collection: T,
   data: DBUserCollections[T]
 ): Promise<DocRef<DBUserCollections[T]>> => {
-  const ref = refUser(user).collection(collection);
-  return ref.add(data) as Promise<DocRef<DBUserCollections[T]>>;
+  return refUserCol(user, collection).add(data);
 };
 
-export { addUser, addUserData, refUser };
+const delUserData = <T extends keyof DBUserCollections>(
+  user: string,
+  collection: T,
+  id: string
+): void => {
+  refUserCol(user, collection).doc(id).delete().catch(console.error);
+};
+
+const refUser = (id: string): DocRef<DBUser> => {
+  return firestore().collection<DBUser>("Users").doc(id);
+};
+
+const refUserCol = <T extends keyof DBUserCollections>(
+  user: string,
+  collection: T
+): ColRef<DBUserCollections[T]> => {
+  return refUser(user).collection(collection) as ColRef<DBUserCollections[T]>;
+};
+
+export { addUser, addUserData, delUserData, refUser };
