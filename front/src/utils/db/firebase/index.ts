a950da3f790ec7456e2532df66f995e9742ed93b
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-13 18:49:10 -0500

    feat(front): #163 add firebase hooks
    - Add useDbUser to get the user's doc data
    - Add useDbUserData to get an user's collection

--- front/src/utils/db/firebase/index.ts
+++ front/src/utils/db/firebase/index.ts
@@ -1,7 +1,8 @@
 import firestore from "@react-native-firebase/firestore";
+import { useEffect, useState } from "react";
 
 import type { DBUser, DBUserCollections } from "../types";
-import type { ColRef, DocRef } from "./types";
+import type { ColRef, DocRef, Query, QueryFilter } from "./types";
 
 const addUser = async (id: string): Promise<void> => {
   const snapshot = await refUser(id).get();
@@ -39,4 +40,40 @@ const refUserCol = <T extends keyof DBUserCollections>(
   return refUser(user).collection(collection) as ColRef<DBUserCollections[T]>;
 };
 
-export { addUser, addUserData, delUserData, refUser };
+const useDbUser = (id: string): DBUser | undefined => {
+  const [data, setData] = useState<DBUser>();
+
+  useEffect(() => {
+    return refUser(id).onSnapshot((snapshot) => {
+      setData(snapshot.data());
+    });
+  }, [id]);
+
+  return data;
+};
+
+const useDbUserData = <T extends keyof DBUserCollections>(
+  user: string,
+  collection: T,
+  filter?: QueryFilter<DBUserCollections[T]>
+): DBUserCollections[T][] | undefined => {
+  const [data, setData] = useState<DBUserCollections[T][]>();
+
+  useEffect(() => {
+    let ref = refUserCol(user, collection) as Query<DBUserCollections[T]>;
+    filter?.forEach(([field, op, val]) => {
+      ref = ref.where(field, op, val);
+    });
+    const sub = ref.onSnapshot((snapshot) => {
+      setData(snapshot.docs.map((doc) => doc.data()));
+    });
+
+    return () => {
+      sub();
+    };
+  }, [user]);
+
+  return data;
+};
+
+export { addUser, addUserData, delUserData, refUser, useDbUser, useDbUserData };
