65ab2d8ae6216bbe49d4590a8d7dba4d2d34313d
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-04-14 08:59:50 -0500

    feat(front): #158 fix lint in events (#183)
    - Fix lint for events
    - Register event when press button for medicine

--- front/src/utils/analytics/analytics.ts
+++ front/src/utils/analytics/analytics.ts
@@ -1,21 +1,21 @@
 import analytics from "@react-native-firebase/analytics";
 
-const RegisterMedicineGA = async () => {
-  await analytics().logEvent("register_medicine", {
-    event_name: "medicine_registered"
-  });
-};
-
-const RegisterFoodGA = async () => {
+const registerFoodGA = async (): Promise<void> => {
   await analytics().logEvent("register_food", {
-    event_name: "food_registered"
+    eventName: "food_registered"
   });
 };
 
-const RegisterSignal = () => {
-  analytics().logEvent("register_signal", {
-    event_name: "signal_registered"
+const registerMedicineGA = async (): Promise<void> => {
+  await analytics().logEvent("register_medicine", {
+    eventName: "medicine_registered"
   });
 };
 
-export { RegisterMedicineGA, RegisterFoodGA, RegisterSignal };
+const registerSignal = async (): Promise<void> => {
+  await analytics().logEvent("register_signal", {
+    eventName: "signal_registered"
+  });
+};
+
+export { registerFoodGA, registerMedicineGA, registerSignal };
