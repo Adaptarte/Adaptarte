d1d1de70b129845fb3e1f9e7cc6f9266bd680357
Estid Felipe Lozano Reyes <36607963+estidlore@users.noreply.github.com>
2023-04-14 15:15:53 -0500

    feat(front): #187 setup status bar (#188)
    - Show StatusBar and set its colors
    - Split Screen styles so the SafeArea only has sizing

--- front/src/components/Screen/index.tsx
+++ front/src/components/Screen/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { SafeAreaView, ScrollView, StatusBar } from "react-native";
 
+import { colors } from "styles";
+
 import { styles } from "./styles";
 import type { IScreenProps } from "./types";
 
 const Screen = ({ children, style }: IScreenProps): JSX.Element => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar hidden />
-      <ScrollView contentContainerStyle={style}>{children}</ScrollView>
+    <SafeAreaView style={styles.root}>
+      <StatusBar backgroundColor={colors.LIGHT} barStyle={"dark-content"} />
+      <ScrollView contentContainerStyle={[styles.container, style]}>
+        {children}
+      </ScrollView>
     </SafeAreaView>
   );
 };
