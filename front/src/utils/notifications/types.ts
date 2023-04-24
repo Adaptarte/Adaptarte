9490246330ddeca520fa23de9d978f43f4487052
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-24 18:49:11 -0500

    chore(front): #215 setup github actions
    - Add gitattributes with eol=lf
    - Update lock json
    - Update format npm script to run on all files under src
    - Add npm scripts format:check & lint:check to check without changes
    - Format code
    - Add workflow front to check lint & format on front changes

--- front/src/utils/notifications/types.ts
+++ front/src/utils/notifications/types.ts
@@ -1,5 +1,6 @@
 import type { Notification } from "@notifee/react-native";
 
-type INotification = Pick<Notification, "body" | "subtitle"> & Required<Pick<Notification, "id" | "title">>;
+type INotification = Pick<Notification, "body" | "subtitle"> &
+  Required<Pick<Notification, "id" | "title">>;
 
 export type { INotification };
