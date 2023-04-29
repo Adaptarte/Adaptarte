96674e58cd0b49fceb685eb68a0c54478e426861
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-04-29 11:02:37 -0500

    feat(front): #132 create acc with email/password
    - add Input for username
    - add create account in auth
    - add button for a space to create account

--- front/src/views/SignIn/index.tsx
+++ front/src/views/SignIn/index.tsx
@@ -7,13 +7,35 @@ import { Img } from "components/Img";
 import { Input } from "components/Input";
 import { Screen } from "components/Screen";
 import { Text } from "components/Text";
-import { signInEmailPassword, signInGoogle } from "utils/auth";
+import {
+  signInEmailPassword,
+  signInGoogle,
+  signUpEmailPassword,
+  updateDisplayname
+} from "utils/auth";
 
 import { btnVars, styles, textVars } from "./styles";
 
 const SignIn = (): JSX.Element => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  const [openRegister, setOpenRegister] = useState(false);
+
+  const handleSignUpEmailPassword = (): void => {
+    if (username !== "" || email !== "" || password !== "") {
+      signUpEmailPassword(email, password)
+        .then(() => {
+          updateDisplayname(username)
+            .then(() => console.log("Created account"))
+            .catch(console.error);
+        })
+        .catch(console.error);
+    } else {
+      console.error("Relleno los campos");
+    }
+  };
 
   const handleSignInEmailPassword = (): void => {
     if (email !== "" || password !== "") {
@@ -21,7 +43,18 @@ const SignIn = (): JSX.Element => {
         .then(() => {
           console.log("Signed in with email/password");
         })
-        .catch(console.error);
+        .catch((err) => {
+          const errMExpected =
+            "[auth/user-not-found] There is no user record corresponding to " +
+            "this identifier. The user may have been deleted.";
+          const errorMessage = err.message;
+          if (errorMessage == errMExpected) {
+            setIsRegister(true);
+          }
+          console.error(err);
+        });
+    } else {
+      console.error("Relleno los campos");
     }
   };
 
@@ -33,6 +66,10 @@ const SignIn = (): JSX.Element => {
       .catch(console.error);
   }, []);
 
+  const changeUsername = (value: React.SetStateAction<string>): void => {
+    setUsername(value);
+  };
+
   const changeEmail = (value: React.SetStateAction<string>): void => {
     setEmail(value);
   };
@@ -41,21 +78,36 @@ const SignIn = (): JSX.Element => {
     setPassword(value);
   };
 
+  const openSignUp = (): void => {
+    setOpenRegister(!openRegister);
+    setIsRegister(false);
+  };
+
   return (
     <Screen>
       <Text variant={textVars.appName}>{"Adaptarte"}</Text>
       <View style={styles.header}>
         <Img src={"helloCuidador"} style={styles.img} />
       </View>
-      <Text variant={textVars.signIn}>{"Inicia sesión"}</Text>
+      <Text variant={textVars.signIn}>
+        {openRegister ? "Crear cuenta" : "Inicia sesión"}
+      </Text>
+      {openRegister && (
+        <Input onChange={changeUsername} label={"Nombre de usuario"} />
+      )}
       <Input onChange={changeEmail} label={"Correo electrónico"} />
       <Input onChange={changePassword} label={"Contraseña"} secure />
+      {isRegister && (
+        <Button onPress={openSignUp}>{"¿Quieres crear una cuenta?"}</Button>
+      )}
       <Button
-        onPress={handleSignInEmailPassword}
+        onPress={
+          openRegister ? handleSignUpEmailPassword : handleSignInEmailPassword
+        }
         style={styles.btn}
         variant={btnVars.signIn}
       >
-        {"Iniciar sesión"}
+        {openRegister ? "Crear cuenta" : "Inicia sesión"}
       </Button>
       <View style={styles.hr} />
       <Button
