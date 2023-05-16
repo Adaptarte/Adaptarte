43e68e178b2624479c0b9b32e27b8feed7cfcb81
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-05-16 18:45:28 -0500

    chore(back): #259 setup babel
    - Config babel and ts base url
    - Set eslint rule simple-import-sort/imports
    - Use absolute path for routes
    - Fix import of firebase-functions

--- packages/back/.eslintrc.js
+++ packages/back/.eslintrc.js
@@ -3,5 +3,24 @@ module.exports = {
     project: ["./tsconfig.json"],
     tsconfigRootDir: __dirname
   },
-  root: false
+  root: false,
+  rules: {
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Side effect imports
+          ["^\\u0000"],
+          // Node.js builtins
+          ["^node:"],
+          // External packages
+          ["^@?\\w"],
+          // Internal packages
+          ["^(config|controllers|middleware|models|routes|services|utils)"],
+          // Relative imports
+          ["^\\."]
+        ]
+      }
+    ]
+  }
 };
--- packages/back/src/app.ts
+++ packages/back/src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 
-import { examsRouter } from "./routes/exams";
-import { foodRouter } from "./routes/food";
-import { medicineRouter } from "./routes/medicine";
+import { examsRouter } from "routes/exams";
+import { foodRouter } from "routes/food";
+import { medicineRouter } from "routes/medicine";
 
 const app = express();
 app.use("/exams", examsRouter);
