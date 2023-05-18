adf6a14618a2bf515d36b8e07a5422378c56b7f0
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-05-18 18:14:30 -0500

    chore(front): #262 optimize common imgs
    - Remove unused imgs
    - Replace white backgrounds by transparent
    - Resize images down to 256px
    - Convert images to webp
    - Rename img Hello_cuidador to Care

--- packages/front/src/assets/imgs/index.ts
+++ packages/front/src/assets/imgs/index.ts
@@ -1,57 +1,39 @@
-import activePause from "./Active_pause.png";
-import back from "./Back.png";
-import calm from "./Calm.png";
-import close from "./Close.png";
-import congrats from "./Congrats.png";
-import dataDose from "./DataDose.png";
-import diet from "./Diet.png";
-import diseaseRegister from "./DiseaseRegister.png";
-import doctor2 from "./Doctor_2.png";
-import drinkingWater from "./DrinkingWater.png";
-import exercise from "./Exercise.png";
-import feeding from "./Feeding.png";
+import back from "./Back.webp";
+import calm from "./Calm.webp";
+import care from "./Care.webp";
+import close from "./Close.webp";
+import dataDose from "./DataDose.webp";
+import diet from "./Diet.webp";
+import diseaseRegister from "./DiseaseRegister.webp";
+import drinkingWater from "./DrinkingWater.webp";
+import exercise from "./Exercise.webp";
+import feeding from "./Feeding.webp";
 import { foodImgs } from "./Foods";
-import helloCuidador from "./Hello_cuidador.png";
-import helloPersona from "./Hello_persona.png";
-import pills from "./Pills.png";
-import plus from "./Plus.png";
-import profile from "./Profile.png";
-import profilePhotoBoy from "./Profile_photo_boy.png";
-import profilePhotoGirl from "./Profile_photo_girl.png";
-import stretching from "./Stretching.png";
-import walking from "./Walking.png";
-import walkingdog from "./WalkingTheDog.png";
-import whatsapp1 from "./Whatsapp_1.png";
-import whatsapp2 from "./Whatsapp_2.png";
-import winner from "./Winner.png";
+import pills from "./Pills.webp";
+import plus from "./Plus.webp";
+import profile from "./Profile.webp";
+import stretching from "./Stretching.webp";
+import walking from "./Walking.webp";
+import walkingdog from "./WalkingTheDog.webp";
 
 const imgs = {
   ...foodImgs,
-  activePause,
   back,
   calm,
+  care,
   close,
-  congrats,
   dataDose,
   diet,
   diseaseRegister,
-  doctor2,
   drinkingWater,
   exercise,
   feeding,
-  helloCuidador,
-  helloPersona,
   pills,
   plus,
   profile,
-  profilePhotoBoy,
-  profilePhotoGirl,
   stretching,
   walking,
-  walkingdog,
-  whatsapp1,
-  whatsapp2,
-  winner
+  walkingdog
 };
 
 export { imgs };
