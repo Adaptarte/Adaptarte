7171d28c15865bcf3b63610a8ffc5874e820791c
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-05-25 12:52:21 -0500

    test(front): #219 add Habit test
    - Add test for component Habit

---
+++ packages/front/src/components/Habit/index.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import { Habit } from ".";
+
+describe("Habit", () => {
+  it("Renders content", () => {
+    expect.assertions(1);
+    render(
+      <Habit bgColor={"ORANGE_TRANSLUCID"} color={"ORANGE"} img={"exercise"}>
+        {"Exercise"}
+      </Habit>
+    );
+
+    const text = screen.queryByText("Exercise");
+    expect(text).toBeOnTheScreen();
+  });
+
+  it("Renders content", () => {
+    expect.assertions(1);
+    const onPress = jest.fn();
+    render(
+      <Habit bgColor={"BLUE"} color={"BLUE"} img={"exercise"} onPress={onPress}>
+        {"Exercise"}
+      </Habit>
+    );
+
+    fireEvent.press(screen.getByText("Exercise"));
+    expect(onPress).toHaveBeenCalledTimes(1);
+  });
+});
