7272bbd4e21460cd13ec5fb6c51930e9b664b9b8
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-06-15 10:53:16 -0500

    refac(front): #305 remove realm utils
    - Load foodIntakes in habits from Rirestore instead of Realm
    - Remove Realm provider and schemas
    - Remove Realm utils and unused types
    - Ignore react-native imports in eslint

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import { AppNavigation } from "navigation/App";
 import { useAuth, UserProvider } from "utils/auth";
-import { RealmProvider } from "utils/db/realm";
 import { useDisuseNotifications } from "utils/notifications";
 import { setupNotifications } from "utils/notifications/setup";
 import { SignIn } from "views/SignIn";
@@ -23,9 +22,7 @@ const App: FC = (): JSX.Element => {
 
   return (
     <UserProvider value={user}>
-      <RealmProvider>
-        <AppNavigation />
-      </RealmProvider>
+      <AppNavigation />
     </UserProvider>
   );
 };
--- packages/front/src/utils/db/realm/types.ts
+++ packages/front/src/utils/db/realm/types.ts
@@ -1,5 +1,3 @@
-import type { ObjectSchema, PropertiesTypes } from "realm";
-
 import type { StrictUnion } from "utils/types";
 
 import type { DBUserCollectionName, DBUserCollections } from "../types";
@@ -8,18 +6,4 @@ type SchemaType<T extends DBUserCollectionName> = StrictUnion<
   DBUserCollections[T] & { readonly id: number }
 >;
 
-type Collection<T extends DBUserCollectionName> = Realm.Collection<
-  Realm.Object<SchemaType<T>> & SchemaType<T>
->;
-
-interface SchemaProps extends PropertiesTypes {
-  id: "int";
-}
-
-interface Schema extends ObjectSchema {
-  name: DBUserCollectionName;
-  primaryKey: "id";
-  properties: SchemaProps;
-}
-
-export type { Collection, SchemaType, Schema };
+export type { SchemaType };
