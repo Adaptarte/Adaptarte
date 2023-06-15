244c5b760c4f9a218c426fab52a013b629dcacf4
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-06-15 12:03:56 -0500

    refac(front): #305 migrate realm types
    - Migrate from type SchemaType to DBDoc

--- packages/front/src/utils/db/realm/types.ts
+++
@@ -1,9 +0,0 @@
-import type { StrictUnion } from "utils/types";
-
-import type { DBUserCollectionName, DBUserCollections } from "../types";
-
-type SchemaType<T extends DBUserCollectionName> = StrictUnion<
-  DBUserCollections[T] & { readonly id: number }
->;
-
-export type { SchemaType };
--- packages/front/src/views/Landing/DailyGoals/MedicineGoal/types.ts
+++ packages/front/src/views/Landing/DailyGoals/MedicineGoal/types.ts
@@ -1,7 +1,7 @@
 import type { DailyGoalProps } from "components/DailyGoal/types";
 
 interface MedicineGoalProps extends Pick<DailyGoalProps, "date" | "done"> {
-  recipeId: number;
+  recipeId: string;
 }
 
 export type { MedicineGoalProps };
