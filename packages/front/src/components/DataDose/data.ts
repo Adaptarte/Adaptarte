a167e2dfbce2489b1dc2e0d95512d1114502715b
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-06-15 17:54:44 -0500

    refac(front): #303 pick dose with weight
    - Add util weightedRandom to  pick elements with distinct probabilities
    - Add groups for each data dose matter
    - Pick data dose using weightedRandom

--- packages/front/src/components/DataDose/data.ts
+++ packages/front/src/components/DataDose/data.ts
@@ -1,6 +1,6 @@
 import type { IDataDose } from "types/dataDoses";
 
-const dataDoses: IDataDose[] = [
+const basic: IDataDose[] = [
   {
     details: "08:00 am - 06:00 pm",
     tip: "Recuerda beber 8 vasos de agua al día"
@@ -11,9 +11,30 @@ const dataDoses: IDataDose[] = [
   }
 ];
 
+const carer: IDataDose[] = [];
+
+const illness: IDataDose[] = [];
+
+const medicine: IDataDose[] = [];
+
+const stress: IDataDose[] = [];
+
+const dataDoses = [basic, carer, illness, medicine, stress];
+const weights = [1, 0, 0, 0, 0];
+
+const weightedRandom = (weights: number[]): number => {
+  const wSum = [weights[0]];
+  for (let i = 1; i < weights.length; i++) {
+    wSum.push(wSum[i - 1] + weights[i]);
+  }
+  const rand = Math.random() * wSum[wSum.length - 1];
+  return wSum.findIndex((val) => val >= rand);
+};
+
 const pickDataDose = (): IDataDose => {
-  const i = Math.floor(Math.random() * dataDoses.length);
-  return dataDoses[i];
+  const group = dataDoses[weightedRandom(weights)];
+  const i = Math.floor(Math.random() * group.length);
+  return group[i];
 };
 
 export { dataDoses, pickDataDose };
