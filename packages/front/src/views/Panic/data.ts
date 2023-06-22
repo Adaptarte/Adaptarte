5f79c16d17744baf921bebed670e440f66260971
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-06-22 12:19:38 -0500

    fix(front) #301 fix error from data
    - fix error of phone numbers when we try to call in iOS

--- packages/front/src/views/Panic/data.ts
+++ packages/front/src/views/Panic/data.ts
@@ -1,7 +1,7 @@
 const contactDirectory = [
-  { name: "Ozami", occupation: "Acompañante", phone: "312 234 43243" },
-  { name: "Estid", occupation: "EPS", phone: "313 243 13723" },
-  { name: "David", occupation: "Médico", phone: "315 616 93423" }
+  { name: "Ozami", occupation: "Acompañante", phone: "31223443243" },
+  { name: "Estid", occupation: "EPS", phone: "31324313723" },
+  { name: "David", occupation: "Médico", phone: "31561693423" }
 ];
 
 export { contactDirectory };
