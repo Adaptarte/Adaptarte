613c8e1e96aa530176552d509a63bd87d090c414
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-06-24 14:56:26 -0500

    fix(front): #298 fix exercise spelling

--- packages/front/src/components/Carousel/CarouselCard/types.ts
+++ packages/front/src/components/Carousel/CarouselCard/types.ts
@@ -1,12 +1,12 @@
 import type { Dispatch, SetStateAction } from "react";
 
-import type { DBExcercise } from "utils/db/types";
+import type { DBExercise } from "utils/db/types";
 import type { ExcerciseProps } from "views/Exercise/types";
 
 interface CarouselCardProps extends ExcerciseProps {
   complete?: boolean;
   setComplete?: Dispatch<SetStateAction<boolean>>;
-  onSave: (data: DBExcercise) => void;
+  onSave: (data: DBExercise) => void;
 }
 
 export type { CarouselCardProps };
