24f1eeeca6a8c4e788043e9c392d4f91a366f6f6
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-06-30 19:40:11 -0500

    feat(front): #350 add editable text
    - Set vertical padding of Input to 2
    - Add component EditableText to allow to modify text information shown

---
+++ packages/front/src/components/EditableText/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "styles";
+
+const styles = StyleSheet.create({
+  button: {
+    marginLeft: 4
+  },
+  container: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row"
+  },
+  field: {
+    flexGrow: 1
+  },
+  text: {
+    borderBottomColor: colors.BLACK,
+    borderBottomWidth: 1,
+    paddingVertical: 2
+  }
+});
+
+export { styles };
