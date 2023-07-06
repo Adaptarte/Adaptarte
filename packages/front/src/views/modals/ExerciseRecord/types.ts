f6a06dd5f0f60dc792facda6dc7ce109455afb73
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-06 17:24:41 -0500

    feat(front): #298 add date to exercise record
    - Add ExerciseRecord translations file
    - Add DatePicker for exercise

--- packages/front/src/views/modals/ExerciseRecord/types.ts
+++ packages/front/src/views/modals/ExerciseRecord/types.ts
@@ -2,7 +2,7 @@ import type { ModalBaseProps } from "components/Modal/types";
 import type { DBExercise } from "utils/db/types";
 
 interface ExerciseRecordProps extends ModalBaseProps {
-  onSave: (data: DBExercise) => void;
+  onSave?: (data: DBExercise) => void;
 }
 
 export type { ExerciseRecordProps };
