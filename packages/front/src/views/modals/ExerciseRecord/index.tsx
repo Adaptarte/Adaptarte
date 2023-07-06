f0d31e1c5f841fccd68ff4f103c90ce579edfcf1
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-06 17:24:41 -0500

    feat(front): #298 add date to exercise record
    - Add ExerciseRecord translations file
    - Add DatePicker for exercise

--- packages/front/src/views/modals/ExerciseRecord/index.tsx
+++ packages/front/src/views/modals/ExerciseRecord/index.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { Button } from "components/Button";
+import { DatePicker } from "components/DatePicker";
 import { Input } from "components/Input";
 import { Modal } from "components/Modal";
 
+import { t } from "./translations";
 import type { ExerciseRecordProps } from "./types";
 
 const ExerciseRecord = ({
@@ -11,42 +13,51 @@ const ExerciseRecord = ({
   setVisible,
   visible
 }: ExerciseRecordProps): JSX.Element => {
-  const [exercise, setExercise] = useState("");
+  const [activity, setActivity] = useState("");
   const [duration, setDuration] = useState("");
+  const [date, setDate] = useState(new Date());
 
   const handleSave = useCallback(() => {
-    onSave({
-      date: new Date(),
+    onSave?.({
+      date,
       duration: parseInt(duration),
-      exercise: exercise
+      exercise: activity
     });
     setVisible?.(false);
-  }, [exercise, duration, setVisible]);
+  }, [activity, duration, setVisible]);
+
+  useEffect(() => {
+    setDate(new Date());
+  }, [visible]);
 
   return (
-    <Modal
-      setVisible={setVisible}
-      title={"Registro de Ejercicios"}
-      visible={visible}
-    >
+    <Modal setVisible={setVisible} title={t().title} visible={visible}>
       <Input
-        label={"Actividad realizada"}
-        onChange={setExercise}
-        value={exercise}
+        label={t().activity.label}
+        onChange={setActivity}
+        placeholder={t().activity.placeholder}
+        value={activity}
       />
       <Input
-        label={"Tiempo de duración en minutos"}
+        label={t().duration.label}
         onChange={setDuration}
+        placeholder={t().duration.placeholder}
         type={"numeric"}
         value={duration}
       />
+      <DatePicker
+        date={date}
+        label={t().date}
+        maxDate={new Date()}
+        onDateChange={setDate}
+      />
       <Button
-        disabled={exercise.length === 0 || duration.length === 0}
+        disabled={activity.length === 0 || isNaN(parseFloat(duration))}
         onPress={handleSave}
         style={{ marginTop: 10 }}
         variant={{ color: "GLAUCOUS", style: "solid" }}
       >
-        {"Registrar"}
+        {t().save}
       </Button>
     </Modal>
   );
---
+++ packages/front/src/views/modals/ExerciseRecord/translations.ts
@@ -0,0 +1,17 @@
+const SPA = {
+  activity: {
+    label: "Actividad realizada",
+    placeholder: "Caminata"
+  },
+  date: "Hora",
+  duration: {
+    label: "Duración (minutos)",
+    placeholder: "10"
+  },
+  save: "Guardar",
+  title: "Registro de ejercicio"
+};
+
+const t = (): typeof SPA => SPA;
+
+export { t };
