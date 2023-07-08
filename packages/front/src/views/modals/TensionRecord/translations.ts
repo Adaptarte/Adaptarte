88b3efebaf087bba6a3a3b8e091bbf27137cbc4a
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-08 00:01:45 -0500

    feat(front): #358 add translations to tension
    - Add translations file for TensionRecord
    - Add test for incorrect data

---
+++ packages/front/src/views/modals/TensionRecord/translations.ts
@@ -0,0 +1,17 @@
+const SPA = {
+  date: "Hora",
+  diastolic: {
+    label: "Presión diastólica",
+    placeholder: "80"
+  },
+  save: "Guardar",
+  systolic: {
+    label: "Presión sistólica",
+    placeholder: "120"
+  },
+  title: "Registro de Tensión"
+};
+
+const t = (): typeof SPA => SPA;
+
+export { t };
