d1e5cf2946a5a9a279f7c078e9eef0a5c929b69f
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-10 10:52:53 -0500

    refac(front): #318 move modal medicineintake
    - Move MedicineIntake to views/modals
    - Rename as MedicineIntakeRecord

--- packages/front/src/views/modals/MedicineIntakeRecord/index.test.tsx
+++ packages/front/src/views/modals/MedicineIntakeRecord/index.test.tsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen } from "@testing-library/react-native";
 
 import { getRecipeById } from "utils/medicine";
 
-import { MedicineIntake } from ".";
+import { MedicineIntakeRecord } from ".";
 
 describe("MedicineIntake", () => {
   const recipe = getRecipeById("1");
@@ -10,7 +10,7 @@ describe("MedicineIntake", () => {
   it("Render content", () => {
     expect.assertions(2);
     const onSave = jest.fn();
-    render(<MedicineIntake onSave={onSave} recipe={recipe} visible />);
+    render(<MedicineIntakeRecord onSave={onSave} recipe={recipe} visible />);
 
     const text = screen.queryByText(`Medicina: ${recipe.data.medicine}`);
     expect(text).toBeOnTheScreen();
@@ -21,7 +21,7 @@ describe("MedicineIntake", () => {
   it("Save data", () => {
     expect.assertions(1);
     const onSave = jest.fn();
-    render(<MedicineIntake onSave={onSave} recipe={recipe} visible />);
+    render(<MedicineIntakeRecord onSave={onSave} recipe={recipe} visible />);
 
     const saveButton = screen.getByText("Registrar");
     fireEvent.press(saveButton);
