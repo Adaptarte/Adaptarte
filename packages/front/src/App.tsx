2049f4725891f2833cdf169179552b40ee5aa55c
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-12 12:16:51 -0500

    refac(front): #318 rename view signin
    - Rename SignIn as Authentication

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,23 +1,22 @@
-import type { FC } from "react";
 import React from "react";
 
 import { AppNavigation } from "navigation/App";
 import { useAuth, UserProvider } from "utils/auth";
 import { useDisuseNotifications } from "utils/notifications";
 import { setupNotifications } from "utils/notifications/setup";
-import { SignIn } from "views/SignIn";
+import { Authentication } from "views/Authentication";
 import { Splash } from "views/Splash";
 
 setupNotifications();
 
-const App: FC = (): JSX.Element => {
+const App = (): JSX.Element => {
   useDisuseNotifications();
   const user = useAuth();
   if (user === undefined) {
     return <Splash />;
   }
   if (user === null) {
-    return <SignIn />;
+    return <Authentication />;
   }
 
   return (
