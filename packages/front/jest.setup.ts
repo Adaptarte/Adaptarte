7ffc1f1ef97caf3dd03f3437148b80573987339c
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-14 18:31:51 -0500

    test(front): #318 test landing
    - Mock lib @notifee
    - Mock utils useUser & useDB
    - Add translations file
    - Fix panicBtn style
    - Add tests for rendering & navigation

--- packages/front/jest.setup.ts
+++ packages/front/jest.setup.ts
@@ -5,8 +5,15 @@ import "@testing-library/jest-native/extend-expect";
 // Silence the warning: Animated: `useNativeDriver` is not supported because
 // the native animated module is missing
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
-
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
+
+jest.mock("@notifee/react-native", () => ({
+  cancelNotification: jest.fn(() => Promise.resolve()),
+  createTriggerNotification: jest.fn(() => Promise.resolve()),
+  TriggerType: {
+    TIMESTAMP: 0
+  }
+}));
 jest.mock("@react-native-google-signin/google-signin", () => ({
   GoogleSignin: {
     configure: jest.fn()
@@ -20,7 +27,21 @@ jest.mock("utils/auth", () => ({
   signInApple: jest.fn(() => Promise.resolve()),
   signInEmailPassword: jest.fn(() => Promise.resolve()),
   signInGoogle: jest.fn(() => Promise.resolve()),
-  signUpEmailPassword: jest.fn(() => Promise.resolve())
+  signUpEmailPassword: jest.fn(() => Promise.resolve()),
+  useUser: jest.fn(() => ({
+    displayName: "John Doe",
+    uid: "U7i",
+    photoURL: ""
+  }))
+}));
+jest.mock("utils/db", () => ({
+  useDB: jest.fn(() => ({
+    addDoc: jest.fn(),
+    delDoc: jest.fn(),
+    getDocs: jest.fn(() => []),
+    getUser: jest.fn(() => ({})),
+    updateUser: jest.fn()
+  }))
 }));
 
 // Enable excluding hidden elements from the queries by default
---
+++ packages/front/src/views/Landing/index.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import type { TAppViewProps } from "navigation/App/types";
+
+import { Landing } from ".";
+import { t } from "./translations";
+
+describe("Landing", () => {
+  type Nav = TAppViewProps<"Landing">["navigation"];
+  let navigation: Partial<Nav>;
+  beforeEach(() => {
+    navigation = { navigate: jest.fn() };
+    render(
+      <Landing
+        navigation={navigation as Nav}
+        route={{ key: "Landing", name: "Landing" }}
+      />
+    );
+  });
+
+  it("Render properly", () => {
+    expect.assertions(5);
+
+    expect(screen.queryByText(t().dailyGoals)).toBeOnTheScreen();
+    expect(screen.queryByText(t().dailyHabits)).toBeOnTheScreen();
+    expect(screen.queryByText(`¡${t().welcome}, John!`)).toBeOnTheScreen();
+    expect(screen.queryByText(t().panic)).toBeOnTheScreen();
+    expect(screen.queryByTestId("icon-user-circle")).toBeOnTheScreen();
+  });
+
+  it("Navigate to other screens", () => {
+    expect.assertions(3);
+
+    fireEvent.press(screen.getByTestId("icon-user-circle"));
+    expect(navigation.navigate).toHaveBeenLastCalledWith("Profile");
+    fireEvent.press(screen.getByText(t().panic));
+    expect(navigation.navigate).toHaveBeenLastCalledWith("Panic");
+    expect(navigation.navigate).toHaveBeenCalledTimes(2);
+  });
+});
