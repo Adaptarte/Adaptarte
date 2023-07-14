d1647c040f3b59bf1a6a5bab837dfbe382d24271
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-07-14 17:26:23 -0500

    refac(front): #298 clean exercise's code
    - Remove unused props from Carousel & its card
    - Remove redundant state management for done exercise

--- packages/front/src/components/Carousel/CarouselCard/types.ts
+++ packages/front/src/components/Carousel/CarouselCard/types.ts
@@ -1,11 +1,8 @@
-import type { Dispatch, SetStateAction } from "react";
-
 import type { DBExercise } from "utils/db/types";
 import type { ExerciseProps } from "views/Exercise/types";
 
 interface CarouselCardProps extends ExerciseProps {
   complete?: boolean;
-  setComplete?: Dispatch<SetStateAction<boolean>>;
   onSave: (data: DBExercise) => void;
 }
 
