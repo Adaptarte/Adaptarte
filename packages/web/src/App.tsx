401e22dfd157ec40993945d61ce816c306bdc1a8
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-02 17:57:32 -0500

    feat(web): #420 add view auth
    - Add demo view Authentication
    - Create simple router

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Authentication } from "views/Authentication";
+import { Landing } from "views/Landing";
+
+const router = createBrowserRouter([
+  {
+    element: <Landing />,
+    path: "/"
+  }
+]);
 
 const App = (): JSX.Element => {
+  const signedIn = true;
+
   return (
-    <div className={"bg-light min-vh-100 text-dark"}>
-      <p>{"Adaptarte"}</p>
+    <div className={"bg-light min-vh-100 px-4 py-3 text-dark"}>
+      <p className={"h1 text-primary mb-3"}>{"Adaptarte"}</p>
+      {signedIn ? <RouterProvider router={router} /> : <Authentication />}
     </div>
   );
 };
