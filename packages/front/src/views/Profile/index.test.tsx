a0cefa4e338d4fabc0e41055daba4d7ade07c4f7
Ozami Duque Rodriguez <81392048+ozamiduro@users.noreply.github.com>
2023-08-02 17:33:06 -0500

    style(front): #423 improve Profile details
    - fix header color
    - move panic button from Landing to Profile
    - add Profile tests
    - add translation for Profile
    - improve details in style
    - add score in Profile

---
+++ packages/front/src/views/Profile/index.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import type { TAppViewProps } from "navigation/App/types";
+
+import { Profile } from ".";
+import { t } from "./translations";
+
+describe("Profile", () => {
+  type Nav = TAppViewProps<"Profile">["navigation"];
+  let navigation: Partial<Nav>;
+  beforeEach(() => {
+    navigation = { navigate: jest.fn() };
+    render(
+      <Profile
+        navigation={navigation as Nav}
+        route={{ key: "Profile", name: "Profile" }}
+      />
+    );
+  });
+
+  it("Render properly", () => {
+    expect.assertions(2);
+
+    expect(screen.queryByText(t().panic)).toBeOnTheScreen();
+    expect(screen.queryByText("Cerrar sesión")).toBeOnTheScreen();
+  });
+
+  it("Navigate to other screens", () => {
+    expect.assertions(2);
+
+    fireEvent.press(screen.getByText(t().panic));
+    expect(navigation.navigate).toHaveBeenLastCalledWith("Panic");
+    expect(navigation.navigate).toHaveBeenCalledTimes(1);
+  });
+});
--- packages/front/src/views/Profile/index.tsx
+++ packages/front/src/views/Profile/index.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback } from "react";
+import { View } from "react-native";
 
 import { Button } from "components/Button";
 import { Screen } from "components/Screen";
+import type { TAppViewProps } from "navigation/App/types";
 import { signOut, useUser } from "utils/auth";
 import { useDB } from "utils/db";
 import type { DBUser } from "utils/db/types";
@@ -10,8 +12,11 @@ import { BasicInfo } from "./BasicInfo";
 import { Diseases } from "./Diseases";
 import { ProfileHeader } from "./Header";
 import { styles } from "./styles";
+import { t } from "./translations";
 
-const Profile = (): JSX.Element => {
+const Profile = ({
+  navigation: { navigate }
+}: TAppViewProps<"Profile">): JSX.Element => {
   const db = useDB();
   const user = useUser();
   const userData = db.getUser();
@@ -27,26 +32,40 @@ const Profile = (): JSX.Element => {
     []
   );
 
+  const goToPanic = useCallback(() => {
+    navigate("Panic");
+  }, [navigate]);
+
   return (
-    <Screen>
+    <Screen bg={"LIGHT"} style={{ padding: 0 }}>
       <ProfileHeader name={user.displayName} photo={user.photoURL} />
-      <Diseases
-        diseases={userData?.diseases ?? {}}
-        key={JSON.stringify(userData?.diseases)}
-        onChange={handleChangeDiseases}
-      />
-      <BasicInfo
-        data={userData?.basicInfo}
-        key={JSON.stringify(userData?.basicInfo)}
-        onChange={handleChangeBasicInfo}
-      />
-      <Button
-        onPress={signOut}
-        style={styles.signOut}
-        variant={{ style: "solid" }}
-      >
-        {"Cerrar sesión"}
-      </Button>
+      <View style={styles.container}>
+        <Diseases
+          diseases={userData?.diseases ?? {}}
+          key={JSON.stringify(userData?.diseases)}
+          onChange={handleChangeDiseases}
+        />
+        <BasicInfo
+          data={userData?.basicInfo}
+          key={JSON.stringify(userData?.basicInfo)}
+          onChange={handleChangeBasicInfo}
+        />
+        <Button
+          onPress={goToPanic}
+          style={styles.panicBtn}
+          variant={{ color: "RED_LIGHT", style: "solid" }}
+        >
+          {t().panic}
+        </Button>
+        <View style={styles.hr} />
+        <Button
+          onPress={signOut}
+          style={styles.signOut}
+          variant={{ style: "solid" }}
+        >
+          {"Cerrar sesión"}
+        </Button>
+      </View>
     </Screen>
   );
 };
