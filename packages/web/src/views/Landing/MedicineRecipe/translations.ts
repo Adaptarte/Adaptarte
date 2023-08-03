4c9e2683ba4d657acf2827183f96594e81f6e741
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-03 17:33:48 -0500

    feat(web): #420 show medicine recipes
    - Add MedicineRecipe for with recipe info & edit and delete actions
    - Show demo medicine recipe

---
+++ packages/web/src/views/Landing/MedicineRecipe/translations.ts
@@ -0,0 +1,12 @@
+const SPA = {
+  delete: "Eliminar",
+  details: "Detalles",
+  edit: "Editar",
+  interval: "Intervalo (horas)",
+  medicine: "Medicina",
+  takeFrom: "Fecha inicial"
+};
+
+const t = (): typeof SPA => SPA;
+
+export { t };
