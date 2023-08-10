b71274beccb41203453c6d991abdb8bc49e19018
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-10 14:52:34 -0500

    feat(web): #437 add db listeners
    - Add util timestampsToDate to parse dates
    - Type DBDoc as possibly undefined
    - Add util listenCol & listenDoc to listen collections & docs

--- packages/web/src/utils/firebase/firestore.ts
+++ packages/web/src/utils/firebase/firestore.ts
@@ -1,4 +1,9 @@
-import type { DocumentData, DocumentSnapshot } from "firebase/firestore";
+import type {
+  DocumentData,
+  DocumentSnapshot,
+  Query,
+  QueryConstraint,
+} from "firebase/firestore";
 import {
   addDoc as dbAddDoc,
   collection,
@@ -6,6 +11,9 @@ import {
   doc,
   getDoc as dbGetDoc,
   getDocs as dbGetCol,
+  onSnapshot,
+  query,
+  Timestamp,
   updateDoc as dbUpdateDoc,
 } from "firebase/firestore";
 
@@ -25,25 +33,65 @@ const delDoc = async (path: string): Promise<void> => {
   await deleteDoc(ref);
 };
 
-const getCol = async <T extends object>(path: string): Promise<DBDoc<T>[]> => {
-  const ref = collection(db, path);
-  const snapshot = await dbGetCol(ref);
+const getCol = async <T extends object>(
+  path: string,
+  filter?: QueryConstraint[],
+): Promise<DBDoc<T>[]> => {
+  const ref = collection(db, path) as Query<T, T>;
+  const filtered = filter === undefined ? ref : query(ref, ...filter);
+  const snapshot = await dbGetCol(filtered);
   return snapshot.docs.map((doc) => getDocData<T>(doc));
 };
 
 const getDoc = async <T extends object>(path: string): Promise<DBDoc<T>> => {
   const ref = doc(db, path);
   const snapshot = await dbGetDoc(ref);
-  return getDocData<T>(snapshot);
+  return getDocData(snapshot);
 };
 
 const getDocData = <T extends object>(
   doc: DocumentSnapshot<DocumentData>,
 ): DBDoc<T> => {
   return {
-    data: doc.data() as T,
+    data: timestampsToDate(doc.data()) as T | undefined,
     id: doc.id,
   };
+};
+
+const listenCol = <T extends object>(
+  path: string,
+  listener: (docs: DBDoc<T>[]) => void,
+  filter?: QueryConstraint[],
+): (() => void) => {
+  const ref = collection(db, path) as Query<T, T>;
+  const filtered = filter === undefined ? ref : query(ref, ...filter);
+  const unsubscribe = onSnapshot(filtered, (snapshot) => {
+    listener(snapshot.docs.map((doc) => getDocData(doc)));
+  });
+  return unsubscribe;
+};
+
+const listenDoc = <T extends object>(
+  path: string,
+  listener: (doc: DBDoc<T>) => void,
+): (() => void) => {
+  const ref = doc(db, path);
+  const unsubscribe = onSnapshot(ref, (snapshot) => {
+    snapshot.data();
+    listener(getDocData(snapshot));
+  });
+  return unsubscribe;
+};
+
+const timestampsToDate = <T>(obj: T): T => {
+  if (obj instanceof Timestamp) {
+    return obj.toDate() as T;
+  } else if (typeof obj === "object" && obj !== null) {
+    for (let key in obj) {
+      obj[key] = timestampsToDate(obj[key]);
+    }
+  }
+  return obj;
 };
 
 const updateDoc = async <T extends object>(
@@ -54,4 +102,4 @@ const updateDoc = async <T extends object>(
   return dbUpdateDoc(ref, data);
 };
 
-export { addDoc, delDoc, getCol, getDoc, updateDoc };
+export { addDoc, delDoc, getCol, getDoc, listenCol, listenDoc, updateDoc };
