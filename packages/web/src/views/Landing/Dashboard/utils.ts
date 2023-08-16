44727f7c02e237a73688ad365464633e51b6e7a7
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-16 11:48:22 -0500

    feat(web): #470 add calms chart
    - Generalize getFoodsChartData as docsToLineChartByDate
    - Add LineChart for calms

--- packages/web/src/views/Landing/Dashboard/utils.ts
+++ packages/web/src/views/Landing/Dashboard/utils.ts
@@ -1,17 +1,17 @@
-import type { DBDoc, DBFoodIntake } from "utils/db/types";
+import type { DBDoc } from "utils/db/types";
 
 import type { LineChartProps } from "./LineChart/types";
 
-const getFoodsChartData = (
-  foods: DBDoc<DBFoodIntake>[],
+const docsToLineChartByDate = (
+  docs: DBDoc<{ date: Date }>[],
 ): Omit<LineChartProps, "title"> => {
-  const foodDates = foods.map((food) =>
-    food.data.date.toLocaleDateString(undefined, {
+  const texts = docs.map((doc) =>
+    doc.data.date.toLocaleDateString(undefined, {
       day: "numeric",
       month: "short",
     }),
   );
-  const foodsPerDate = foodDates.reduce<Record<string, number>>((acc, date) => {
+  const counts = texts.reduce<Record<string, number>>((acc, date) => {
     acc[date] = (acc[date] ?? 0) + 1;
     return acc;
   }, {});
@@ -19,11 +19,11 @@ const getFoodsChartData = (
     data: [{ data: [] }],
     labels: [],
   } as Omit<LineChartProps, "title">;
-  return Object.entries(foodsPerDate).reduce((acc, [key, val]) => {
+  return Object.entries(counts).reduce((acc, [key, val]) => {
     acc.labels.push(key);
     acc.data[0].data.push(val);
     return acc;
   }, res);
 };
 
-export { getFoodsChartData };
+export { docsToLineChartByDate };
