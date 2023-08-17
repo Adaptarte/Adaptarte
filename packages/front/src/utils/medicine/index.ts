676490d9326290127cfa9f1b7635f7974bb84d51
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-17 17:32:27 -0500

    refac(front): #487 remove old medicine utils
    - Remove old medicine utils based on fixed medicine recipes

--- packages/front/src/utils/medicine/index.ts
+++ packages/front/src/utils/medicine/index.ts
@@ -1,7 +1,6 @@
-import { addTime, compareDates, setDayTime } from "utils/date";
+import { addTime, setDayTime } from "utils/date";
 import type { DBDoc, DBMedicineIntake, DBMedicineRecipe } from "utils/db/types";
 
-import { recipes } from "./data";
 import type { MedicineRecipeGoals, MedicineRecipeIntakes } from "./types";
 
 const groupMedicines = (
@@ -46,44 +45,4 @@ const getMedicineGoals = (
   });
 };
 
-const getLastIntakes = (
-  data: DBDoc<DBMedicineIntake>[],
-): DBDoc<DBMedicineIntake>[] => {
-  return data.reduce<DBDoc<DBMedicineIntake>[]>((acc, curr) => {
-    const i = parseInt(curr.data.recipe);
-    const accDate = acc[i]?.data.date;
-    if (
-      accDate === undefined ||
-      compareDates(curr.data.date, accDate, true) > 0
-    ) {
-      acc[i] = curr;
-    }
-    return acc;
-  }, []);
-};
-
-const getNextIntake = (
-  recipe: DBMedicineRecipe,
-  last?: DBMedicineIntake,
-): Date => {
-  if (last === undefined) {
-    return new Date(recipe.date);
-  }
-  return addTime(new Date(last.date), recipe.interval, "hour");
-};
-
-const getRecipeById = (id: string): DBDoc<DBMedicineRecipe> => {
-  const expected = recipes[parseInt(id) - 1];
-  if (id !== expected.id) {
-    throw new Error("Unexpected recipe id");
-  }
-  return expected;
-};
-
-export {
-  getLastIntakes,
-  getMedicineGoals,
-  getNextIntake,
-  getRecipeById,
-  recipes,
-};
+export { getMedicineGoals };
