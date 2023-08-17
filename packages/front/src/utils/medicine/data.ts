91757c0bfc23c0cb6dd15a1c6859ece228e8eb91
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-17 13:24:57 -0500

    feat(front): #487 add util getmedicinegoals
    - Remove prop amount of DBMedicineRecipe & rename takeFrom as date
    - Add util getMedicineGoals to get recipes with today's intakes & goals

--- packages/front/src/utils/medicine/data.ts
+++ packages/front/src/utils/medicine/data.ts
@@ -3,21 +3,18 @@ import type { DBDoc, DBMedicineRecipe } from "utils/db/types";
 const recipes: DBDoc<DBMedicineRecipe>[] = [
   {
     data: {
-      amount: 0,
+      date: new Date(2023, 3, 1, 6),
       details: "Tomar 30 min antes del desayuno",
       interval: 24,
       medicine: "Levotiroxina x100 Mcgr",
-      takeFrom: new Date(2023, 3, 1, 6),
     },
     id: "1",
   },
   {
     data: {
-      amount: 0,
-      details: "",
+      date: new Date(2023, 3, 1, 14),
       interval: 12,
       medicine: "Amlodipino x 5mg",
-      takeFrom: new Date(2023, 3, 1, 14),
     },
     id: "2",
   },
--- packages/front/src/utils/medicine/index.ts
+++ packages/front/src/utils/medicine/index.ts
@@ -1,7 +1,51 @@
-import { addTime, compareDates } from "utils/date";
+import { addTime, compareDates, setDayTime } from "utils/date";
 import type { DBDoc, DBMedicineIntake, DBMedicineRecipe } from "utils/db/types";
 
 import { recipes } from "./data";
+import type { MedicineRecipeGoals, MedicineRecipeIntakes } from "./types";
+
+const groupMedicines = (
+  recipes: DBDoc<DBMedicineRecipe>[],
+  intakes: DBDoc<DBMedicineIntake>[],
+): MedicineRecipeIntakes[] => {
+  const medicines: Record<string, MedicineRecipeIntakes> = {};
+  recipes.reduce((acc, recipe) => {
+    acc[recipe.id] = { intakes: [], recipe };
+    return acc;
+  }, medicines);
+  intakes.forEach((intake) => {
+    medicines[intake.data.recipe].intakes.push(intake);
+  });
+  return Object.values(medicines).map((el) => {
+    el.intakes.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+    return el;
+  });
+};
+
+const getMedicineGoals = (
+  recipes: DBDoc<DBMedicineRecipe>[],
+  intakes: DBDoc<DBMedicineIntake>[],
+): MedicineRecipeGoals[] => {
+  const medicines = groupMedicines(recipes, intakes);
+  return medicines.map((medicine) => {
+    let date = medicine.intakes[0]?.data.date ?? medicine.recipe.data.date;
+    medicine.intakes = medicine.intakes.filter(
+      (el) => el.data.date.getTime() >= today.getTime(),
+    );
+
+    const today = setDayTime(new Date(), 0);
+    const tomorrow = addTime(new Date(today), 1, "day");
+    const goals: Date[] = [];
+    while (goals.length < 2) {
+      date = addTime(new Date(date), medicine.recipe.data.interval, "hour");
+      if (date.getTime() >= tomorrow.getTime()) {
+        break;
+      }
+      goals.unshift(date);
+    }
+    return Object.assign({ goals }, medicine);
+  });
+};
 
 const getLastIntakes = (
   data: DBDoc<DBMedicineIntake>[],
@@ -24,7 +68,7 @@ const getNextIntake = (
   last?: DBMedicineIntake,
 ): Date => {
   if (last === undefined) {
-    return new Date(recipe.takeFrom);
+    return new Date(recipe.date);
   }
   return addTime(new Date(last.date), recipe.interval, "hour");
 };
@@ -37,4 +81,10 @@ const getRecipeById = (id: string): DBDoc<DBMedicineRecipe> => {
   return expected;
 };
 
-export { getLastIntakes, getNextIntake, getRecipeById, recipes };
+export {
+  getLastIntakes,
+  getMedicineGoals,
+  getNextIntake,
+  getRecipeById,
+  recipes,
+};
