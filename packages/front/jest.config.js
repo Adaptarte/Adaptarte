b37128602e01817b19c4761d224675bd7ffd85c5
Jonathan Buitrago Roncancio <jonathanburo@unisabana.edu.co>
2023-10-25 08:17:29 -0500

    fix tests for react swipe gestures
    add ignore for react swipe gestures in jest.config

--- packages/front/jest.config.js
+++ packages/front/jest.config.js
@@ -3,8 +3,9 @@ const esModules = [
   "@react-native",
   "@react-native-firebase",
   "react-native",
+  "react-native-swipe-gestures",
   "react-native-date-picker",
-  "react-native-vector-icons"
+  "react-native-vector-icons",
 ].join("|");
 
 module.exports = {
@@ -17,16 +18,16 @@ module.exports = {
       branches: 50,
       functions: 50,
       lines: 50,
-      statements: 50
-    }
+      statements: 50,
+    },
   },
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js"
+      "<rootDir>/__mocks__/fileMock.js",
   },
   preset: "@testing-library/react-native",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`]
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
 };
